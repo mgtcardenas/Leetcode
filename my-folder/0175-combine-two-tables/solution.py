0b6146b720dcc86d143a8e517b9d14ae5a793b5d
Marco Gutiérrez <29807378+mgtcardenas@users.noreply.github.com>
2024-05-26 19:01:49 +0000

    [LeetCode Sync] Runtime - 690 ms (26.96%), Memory - 67.5 MB (9.04%)

--- my-folder/0175-combine-two-tables/solution.py
+++ my-folder/0175-combine-two-tables/solution.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
-    resultDf = person.merge(address, how='left', on = 'personId')
-    return resultDf[['firstName', 'lastName', 'city', 'state']]
+    return person.merge(address, how='left', on = 'personId')[['firstName', 'lastName', 'city', 'state']]
